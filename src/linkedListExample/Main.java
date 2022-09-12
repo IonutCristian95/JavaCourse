5a541e7743ca8432a646c317bbc14c1da7e7e1cb
IonutCristian95 <gifuionut9519@yahoo.com>
2022-09-12 11:14:30 +0200

    linked List Java Example

---
+++ src/linkedListExample/Main.java
@@ -0,0 +1,39 @@
+package linkedListExample;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(6);
+
+
+        myLinkedList.add(node1);
+        myLinkedList.add(node2);
+        myLinkedList.add(node3);
+
+        myLinkedList.printMyLinkedList();
+
+        Node node4 = new Node(15);
+        myLinkedList.add(node4);
+        myLinkedList.printMyLinkedList();
+
+        System.out.println(myLinkedList.lookup(4) == null);
+        System.out.println(myLinkedList.lookup(15) != null);
+
+        myLinkedList.insert(0, 8);
+        myLinkedList.printMyLinkedList();
+        myLinkedList.insert(200, 80);
+        myLinkedList.printMyLinkedList();
+        myLinkedList.insert(2, 4);
+        myLinkedList.printMyLinkedList();
+
+
+        myLinkedList.delete(80);
+        myLinkedList.printMyLinkedList();
+
+        myLinkedList.insert(70, 20);
+        myLinkedList.insert(70, 21);
+        myLinkedList.printMyLinkedList();
+    }
+}
---
+++ src/linkedListExample/MyLinkedList.java
@@ -0,0 +1,89 @@
+package linkedListExample;
+
+public class MyLinkedList {
+    private Node head;
+    private Node currentNode;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.currentNode = null;
+    }
+
+    public MyLinkedList(Node head) {
+        this.head = head;
+        this.currentNode = this.head;
+    }
+
+    public void add(Node node){
+        if(this.head == null){
+            this.head = node;
+            this.currentNode = this.head;
+        }else{
+            this.currentNode.setNext(node);
+            this.currentNode = node;
+        }
+    }
+
+    public void printMyLinkedList(){
+        Node temp = this.head;
+        while(temp!=null){
+            System.out.print(temp.getValue() + " ");
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+
+    public Node lookup(int value){
+        Node temp = this.head;
+        while(temp!=null){
+            if(temp.getValue() == value)
+                return temp;
+            temp = temp.getNext();
+        }
+        return null;
+    }
+
+    public void insert(int position, int value){
+        Node temp = new Node(value);
+        if(position == 0){
+            temp.setNext(this.head);
+            this.head = temp;
+            return;
+        }
+        Node currentNode = this.head.getNext();
+        Node previousNode = this.head;
+        int currentPosition = 1;
+        while(currentNode!=null){
+            if(currentPosition == position){
+                previousNode.setNext(temp);
+                temp.setNext(currentNode);
+                return;
+            }
+            currentPosition++;
+            currentNode = currentNode.getNext();
+            previousNode = previousNode.getNext();
+        }
+        //happens only if position is bigger than the size of myLinkedList
+        add(temp);
+    }
+
+    public void delete(int value){
+        Node currNode = this.head.getNext();
+        Node previousNode = this.head;
+        if(this.head.getValue() == value){
+            this.head = this.head.getNext();
+            return;
+        }
+
+        while(currNode!=null){
+            if(currNode.getValue() == value){
+                if(currNode.getNext()==null){
+                    this.currentNode = previousNode;
+                }
+                previousNode.setNext(currNode.getNext());
+            }
+            previousNode = previousNode.getNext();
+            currNode = currNode.getNext();
+        }
+    }
+}
