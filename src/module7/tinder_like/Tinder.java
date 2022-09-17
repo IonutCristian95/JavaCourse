892975a70419ff3d93a2d829bd8d8c8ebe85bb5d
IonutCristian95 <gifuionut9519@yahoo.com>
2022-09-17 13:04:22 +0200

    module 7 - tinder-like update //To Do: tests

--- src/module7/tinder_like/Tinder.java
+++ src/module7/tinder_like/Tinder.java
@@ -1,8 +1,9 @@
 package module7.tinder_like;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.TreeMap;
 
 public class Tinder {
     private HashMap<Utente, HashSet<Interesse>> usersDataBase;
@@ -32,12 +33,10 @@ public class Tinder {
     }
 
     public Utente commonInterests(Utente utente) throws UserNotFoundException{
-        if (utente == null || usersDataBase.size()==1 || usersDataBase.size()==0){
+        if (!isValidUser(utente)){
             return null;
         }
-        if (usersDataBase.get(utente)==null){
-            throw new UserNotFoundException(utente);
-        }
+
         HashSet<Interesse> utenteInterests = utente.getUserInterests();
         HashSet<Interesse> comparedUserInterests;
         HashSet<Interesse> tempInterests;
@@ -57,4 +56,46 @@ public class Tinder {
         }
         return userCommonInterests;
     }
+
+    public TreeMap<Utente, Integer> getBestMatches(Utente utente) throws UserNotFoundException{
+        if (!isValidUser(utente)){
+            return null;
+        }
+        HashMap<Utente, Integer> tempUsers = new HashMap<>();
+        HashSet<Interesse> utenteInterests = utente.getUserInterests();
+        HashSet<Interesse> tempInterests;
+
+        for (Utente utente1 : usersDataBase.keySet()) {
+            if(utente.equals(utente1)){
+                continue;
+            }
+            tempInterests = new HashSet<>(utenteInterests);
+            tempInterests.retainAll(utente1.getUserInterests());
+            tempUsers.put(utente1, tempInterests.size());
+        }
+
+        TreeMap<Utente, Integer> ordered = new TreeMap<>(new Comparator<Utente>() {
+            @Override
+            public int compare(Utente o1, Utente o2) {
+                if (tempUsers.get(o1) > tempUsers.get(o2)) return -1;
+                else if(tempUsers.get(o1) < tempUsers.get(o2)) return 1;
+                else return 0;
+            }
+        });
+
+        ordered.putAll(tempUsers);
+
+        return ordered;
+    }
+
+    private boolean isValidUser(Utente utente) throws UserNotFoundException{
+        if (utente == null || usersDataBase.size()>=1){
+            return false;
+        }
+        if (usersDataBase.get(utente)==null){
+            throw new UserNotFoundException(utente);
+        }
+        return true;
+    }
+
 }
