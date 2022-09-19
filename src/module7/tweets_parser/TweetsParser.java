928382d735c4c537bdb8f83b552a20f8cdc09426
IonutCristian95 <gifuionut9519@yahoo.com>
2022-09-19 14:47:15 +0200

    module 7 - tweetsParser final update - using english stopwords

--- src/module7/tweets_parser/TweetsParser.java
+++ src/module7/tweets_parser/TweetsParser.java
@@ -9,8 +9,14 @@ import java.util.*;
 
 public class TweetsParser {
     public static void main(String[] args) {
-        Path pathToFile = Paths.get("C:\\Users\\Heinrich\\Desktop\\JavaProjects\\testProgetto\\src\\module7\\tweets_parser\\realdonaldtrump.csv");
+        Path pathToFile = Paths.get("src\\module7\\tweets_parser\\resources\\realdonaldtrump.csv");
+        Path pathToStopWords = Paths.get("src\\module7\\tweets_parser\\resources\\english_stopwords.txt");
         Map<String, Integer> wordsMap = new HashMap<>();
+        HashSet<String> stopWords = new HashSet<>();
+
+        //Get the stopwords
+        getStopWords(stopWords, pathToStopWords);
+
 
         try (BufferedReader br = Files.newBufferedReader(pathToFile)){
             br.readLine(); //Get rid of the first row in the document : id, link, content, date, retweets, favorites, mentions, hashtags
@@ -23,7 +29,7 @@ public class TweetsParser {
                 tempString = line.split(" ");
 
                 for (String s : tempString) {
-                    if (s.isEmpty() || s.isBlank()) continue;
+                    if (s.isEmpty() || s.isBlank() || stopWords.contains(s.toLowerCase())) continue;
                     if (!wordsMap.containsKey(s)){
                         wordsMap.put(s, 1);
                     }else{
@@ -47,6 +53,16 @@ public class TweetsParser {
 
         } catch (IOException ioe){
             ioe.printStackTrace();
+        }
+    }
+
+    private static void getStopWords(HashSet<String> temp, Path pathToStopWordsFile){
+        try (BufferedReader br2 = Files.newBufferedReader(pathToStopWordsFile)){
+            while (br2.ready()){
+                temp.add(br2.readLine().strip());
+            }
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
